4cdf3b019e7b934057a5251ac65babd48c5eee64
vladpacc <48735802+vladpacc@users.noreply.github.com>
2020-04-28 15:54:43 -0500

    Create ebs.tf

---
+++ ebs.tf
@@ -0,0 +1,29 @@
+resource "aws_ebs_volume" "ebs_vol" {
+  availability_zone = "us-west-2"
+  size              = 100
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web.id}"
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-21f78e11"
+  availability_zone = "us-west-2"
+  instance_type     = "t2.large"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 1
+}
