5fb6a6ccc15c8f8eaf73b3676e553abe01ca3b87
vladpacc <vladpylypenko13@gmail.com>
2020-05-08 00:19:52 +0000

    updates

---
+++ Azure/variable.tf
@@ -0,0 +1,4 @@
+variable    "vnet_name" {}
+variable    "location" {}
+variable    "address_space" {}
+variable "resource_group_name" {}
---
+++ Azure/vpc.tfvars
@@ -0,0 +1,4 @@
+vnet_name = "dev"
+location  = "westeurope"
+address_space = "10.0.0.0/16"
+resource_group_name = "example"
